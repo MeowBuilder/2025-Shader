ac3a3868407b58a9e79e5c74de8f41b35e14cb84
MeowBuilder <96215662+MeowBuilder@users.noreply.github.com>
2025-09-09 14:10:27 +0900

    123

---
+++ SimpleGame/Shaders/SolidRect.fs
@@ -0,0 +1,15 @@
+#version 330
+
+layout(location=0) out vec4 FragColor;
+
+in vec4 v_Color;
+
+uniform vec4 u_Color;
+
+void main()
+{
+	//FragColor = vec4(u_Color.r, u_Color.g, u_Color.b, u_Color.a);
+	FragColor = v_Color;
+	if	(v_Color.b > 0.5 || v_Color.r > 0.5)
+		discard;
+}
